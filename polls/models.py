63135a91c3c7f5820963a2ef612f3fda9b6fe1a9
Luis Martel <martel19.lm@gmail.com>
2023-02-04 02:11:08 +0100

    Small Polls application

---
+++ polls/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib import admin
+import datetime
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200, help_text="Write a Question")
+    pub_date = models.DateField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+    @admin.display(
+        boolean=True,
+        ordering='pub-date',
+        description='published recently?'
+    )
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200, help_text="Write a Choice response ")
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
